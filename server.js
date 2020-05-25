7440ad7ffb9a594f98b6662d9cdfed8b98451d0b
Marine Heckler <marine.heckler@gmail.com>
2020-05-25 13:03:53 +0200

    Connect server with db.

--- server.js
+++ server.js
@@ -21,10 +21,8 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'Marine',
-        password: '',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
